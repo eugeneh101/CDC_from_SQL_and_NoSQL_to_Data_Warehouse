f76126b7b6425c231a452bc43768d053f14f656d
Eugene <eugeneh101@DESKTOP-6EJMNGA.localdomain>
2022-12-17 22:06:31 -0500

    upgrades to Lambda handler

--- source/load_s3_files_from_dynamodb_stream_to_redshift_lambda/handler.py
+++ source/load_s3_files_from_dynamodb_stream_to_redshift_lambda/handler.py
@@ -3,21 +3,24 @@ import time
 
 import boto3
 
-s3_bucket = boto3.resource("s3").Bucket(
-    os.environ["S3_FOR_DYNAMODB_STREAM_TO_REDSHIFT"]
-)
-redshift_data_client = boto3.client("redshift-data")
 
+AWS_REGION = os.environ["AWSREGION"]
+
+s3_client = boto3.client("s3")
+S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT = os.environ["S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT"]
+UNPROCESSED_DYNAMODB_STREAM_FOLDER = os.environ["UNPROCESSED_DYNAMODB_STREAM_FOLDER"]
+PROCESSED_DYNAMODB_STREAM_FOLDER = os.environ["PROCESSED_DYNAMODB_STREAM_FOLDER"]
+
+redshift_data_client = boto3.client("redshift-data")
 REDSHIFT_CLUSTER_NAME = os.environ["REDSHIFT_ENDPOINT_ADDRESS"].split(".")[0]
 REDSHIFT_ROLE_ARN = os.environ["REDSHIFT_ROLE_ARN"]
 REDSHIFT_USER = os.environ["REDSHIFT_USER"]
 REDSHIFT_DATABASE_NAME = os.environ["REDSHIFT_DATABASE_NAME"]
 REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC = os.environ["REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC"]
 REDSHIFT_TABLE_NAME_FOR_DYNAMODB_CDC = os.environ["REDSHIFT_TABLE_NAME_FOR_DYNAMODB_CDC"]
-AWS_REGION = os.environ["AWSREGION"]
 
 
-def execute_sql_statement(sql_statement):
+def execute_sql_statement(sql_statement: str) -> None:
     response = redshift_data_client.execute_statement(
         ClusterIdentifier=REDSHIFT_CLUSTER_NAME,
         Database=REDSHIFT_DATABASE_NAME,
@@ -35,14 +38,35 @@ def execute_sql_statement(sql_statement):
             time.sleep(1)
         elif status == "FAILED":
             print(response)
-            raise  ### figure out useful messge in exception
+            raise  ### figure out useful message in exception
         else:
             print(response)
-            raise  ### figure out useful messge in exception
+            raise  ### figure out useful message in exception
 
 
-def lambda_handler(event, context):
-    dynamodb_stream_s3_files = list(s3_bucket.objects.all())
+def move_s3_file(s3_bucket: str, old_s3_filename: str, new_s3_filename) -> None:
+    s3_client.copy_object(
+        Bucket=s3_bucket,
+        Key=new_s3_filename,
+        CopySource={"Bucket": s3_bucket, "Key": old_s3_filename},
+    )
+    s3_client.delete_object(
+        Bucket=s3_bucket,
+        Key=old_s3_filename,
+    )
+    print(
+        f"Moved s3://{s3_bucket}/{old_s3_filename} to "
+        f"s3://{s3_bucket}/{new_s3_filename}"
+    )
+
+
+def lambda_handler(event, context) -> None:
+    dynamodb_stream_s3_files = s3_client.list_objects_v2(
+        Bucket=S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT,
+        Prefix=f"{UNPROCESSED_DYNAMODB_STREAM_FOLDER}/",
+        Delimiter="/"
+    ).get("Contents", [])
+    dynamodb_stream_s3_files = [dct["Key"] for dct in dynamodb_stream_s3_files]
     if dynamodb_stream_s3_files:
         sql_statements = [
             f"CREATE SCHEMA IF NOT EXISTS {REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC};",
@@ -59,21 +83,37 @@ def lambda_handler(event, context):
         for sql_statement in sql_statements:
             execute_sql_statement(sql_statement=sql_statement)
         for s3_file in dynamodb_stream_s3_files:
-            if "__inserted_or_modified_records__" in s3_file.key:
+            if "__inserted_or_modified_records.json" in s3_file:  # hard coded suffix
                 sql_statement = f"""
                     COPY {REDSHIFT_DATABASE_NAME}.{REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC}.{REDSHIFT_TABLE_NAME_FOR_DYNAMODB_CDC}
-                    FROM 's3://{s3_bucket.name}/{s3_file.key}'
+                    FROM 's3://{S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT}/{s3_file}'
                     REGION '{AWS_REGION}'
                     iam_role '{REDSHIFT_ROLE_ARN}'
                     format as json 'auto';
                 """
                 execute_sql_statement(sql_statement=sql_statement)
-                print(f"Deleting s3://{s3_bucket.name}/{s3_file.key}")
-                s3_file.delete()
-            elif "__no_inserted_or_modified_records__" in s3_file.key:
-                print(f"Deleting s3://{s3_bucket.name}/{s3_file.key}")
-                s3_file.delete()
+                move_s3_file(
+                    s3_bucket=S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT,
+                    old_s3_filename=s3_file,
+                    new_s3_filename=s3_file.replace(
+                        UNPROCESSED_DYNAMODB_STREAM_FOLDER,
+                        PROCESSED_DYNAMODB_STREAM_FOLDER,
+                    ),
+                )
+            elif "__no_inserted_or_modified_records.txt" in s3_file:  # hard coded suffix
+                move_s3_file(
+                    s3_bucket=S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT,
+                    old_s3_filename=s3_file,
+                    new_s3_filename=s3_file.replace(
+                        UNPROCESSED_DYNAMODB_STREAM_FOLDER,
+                        PROCESSED_DYNAMODB_STREAM_FOLDER,
+                    ),
+                )
             else:
-                raise  ### figure out useful messge in exception
+                raise  ### figure out useful message in exception
     else:
-        print("No DynamoDB stream files")
+        print(
+            "No DynamoDB stream files in "
+            f"s3://{S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT}/"
+            f"{UNPROCESSED_DYNAMODB_STREAM_FOLDER}/ folder"
+        )
--- source/write_dynamodb_stream_to_s3_lambda/handler.py
+++ source/write_dynamodb_stream_to_s3_lambda/handler.py
@@ -8,9 +8,9 @@ import boto3
 from boto3.dynamodb.types import TypeDeserializer
 
 s3_bucket = boto3.resource("s3").Bucket(
-    os.environ["S3_FOR_DYNAMODB_STREAM_TO_REDSHIFT"]
+    os.environ["S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT"]
 )
-
+UNPROCESSED_DYNAMODB_STREAM_FOLDER = os.environ["UNPROCESSED_DYNAMODB_STREAM_FOLDER"]
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
@@ -19,7 +19,7 @@ class DecimalEncoder(json.JSONEncoder):
         return super().default(o)
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, context) -> None:
     s3_file_contents = []
     # print(event["Records"])
     print(f"Number of records received from DynamoDB stream: {len(event['Records'])}")
@@ -32,7 +32,8 @@ def lambda_handler(event, context):
             pass
         else:
             raise ValueError(
-                f'''Did not expect DynamoDB stream's `eventName` to be "{record["eventName"]}"'''
+                "Did not expect DynamoDB stream's `eventName` "
+                f'to be "{record["eventName"]}"'
             )
     # print(s3_file_contents)
     s3_file_contents_in_redshift_json_string = "\n".join(
@@ -41,16 +42,18 @@ def lambda_handler(event, context):
     if s3_file_contents_in_redshift_json_string:
         s3_bucket.put_object(
             Key=(
-                f"{datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}"
-                f"__inserted_or_modified_records__{uuid.uuid4()}.json"
+                f"{UNPROCESSED_DYNAMODB_STREAM_FOLDER}/"
+                f"{datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}__{uuid.uuid4()}"
+                "__inserted_or_modified_records.json"  # hard coded suffix
             ),
             Body=s3_file_contents_in_redshift_json_string.encode(),
         )
     else:
         s3_bucket.put_object(
             Key=(
-                f"{datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}"
-                f"__no_inserted_or_modified_records__{uuid.uuid4()}.txt"
+                f"{UNPROCESSED_DYNAMODB_STREAM_FOLDER}/"
+                f"{datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}__{uuid.uuid4()}"
+                "__no_inserted_or_modified_records.txt"  # hard coded suffix
             )
         )
     print(f"Number of records written to S3 file: {len(s3_file_contents)}")
